c95d85f1aa053dfc89280cd3b9f37fca316f8eec
Lars Richter <github@n-develop.com>
2021-02-07 21:15:20 +0100

    Adding some styles, elements and stuff

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,14 +3,111 @@
     <section class="hero is-fullheight-with-navbar is-info">
       <div class="hero-body">
         <div>
-          <p class="title">
-            Frustfrei spenden
+          <p class="title is-1">
+            Simply Donate
           </p>
-          <p class="subtitle">
-            Einfach. Schnell. Lokal.
+          <p class="subtitle is-2">
+            Spenden. Einfach. Lokal. Frustfrei.
           </p>
         </div>
       </div>
+    </section>
+
+    <section class="section is-medium">
+      <article class="media">
+        <figure class="media-left">
+          <p class="image">
+            <img src="https://via.placeholder.com/300">
+          </p>
+        </figure>
+        <div class="media-content">
+          <div class="content">
+            <p class="title is-2">Einfach?</p>
+            <p class="subtitle is-3">
+              Hier findest du den richtigen Abnehmer für deine Sachspenden.
+            </p>
+            <p>
+              Den richtigen Abnehmer für Sachspenden zu finden kann anstrenged sein. Wo fängt beginnt man mit der Suche?
+              Nutzt man eine Kleinanzeigen-Plattform? Wenn ja, woher weiß man, dass die Spende auch wirklich
+              bei Bedürftigen ankommt und nicht im Anschluss einfach gewinnbringend weiterverkauft wird?
+            </p>
+            <p>
+              Oder vielleicht hast du auch schon eine Idee wo du spenden könntest, kannst aber leider keinen
+              Ansprechpartner finden?
+            </p>
+            <p>
+              Hier kommen wir ins Spiel. Wer Gutes tut sollte dabei unterstützt und nicht ausgebremst werden.
+              Auf "Simply Donate" reduzieren wir den Aufwand für dich auf ein Minimum.
+            </p>
+          </div>
+        </div>
+      </article>
+    </section>
+
+
+    <section class="section is-medium" style="background-color: #3298dc;">
+      <article class="media">
+
+        <div class="media-content" style="color: #fff;">
+          <div class="content">
+            <p class="title is-2" style="color: #fff;">Lokal?</p>
+            <p class="subtitle is-3" style="color: #fff;">
+              Gut für deine Gemeinde. Gut für die Umwelt. Gut für uns alle.
+            </p>
+            <p>
+              Warum sollte deine alte Xbox einmal durch die ganze Republik geschickt werden,
+              wenn doch direkt um die Ecke die Fl&uuml;chtlingshilfe, das Jugendzentrum oder das "Kaufhaus mit Herz"
+              genau auf sowas wartet?
+            </p>
+            <p>
+              Potenti dui iaculis velit, fringilla ullamcorper torquent curabitur elit. Pulvinar nec ante aenean,
+              est eu fusce, consequat nisl lorem habitant elit suspendisse. Ut tortor metus, tempus ullamcorper,
+              vehicula lorem euismod venenatis mollis. Diam habitant duis, suscipit nostra, eu etiam orci sollicitudin
+              ligula.
+            </p>
+            <p>
+              Cras commodo metus fusce, dapibus lobortis urna, quisque erat magna vel ullamcorper mattis. Praesent
+              lectus laoreet aenean, senectus amet nec, quisque est lobortis volutpat habitant fringilla porttitor nam.
+              Hendrerit et feugiat semper, torquent leo lacinia, amet urna pellentesque lacus ac lectus orci etiam.
+              Curae euismod praesent, eu vivamus urna placerat ipsum.
+            </p>
+          </div>
+        </div>
+        <figure class="media-right">
+          <p class="image">
+            <img src="https://via.placeholder.com/500x300">
+          </p>
+        </figure>
+      </article>
+    </section>
+    <section class="section is-medium">
+      <article class="media">
+        <figure class="media-left">
+          <p class="image">
+            <img src="https://via.placeholder.com/300">
+          </p>
+        </figure>
+        <div class="media-content">
+          <div class="content">
+            <p class="title is-2">Frustfrei?</p>
+            <p class="subtitle is-3">
+              Keine Verhandlungen. Immer ein gutes Gefühl.
+            </p>
+            <p>
+              Potenti dui iaculis velit, fringilla ullamcorper torquent curabitur elit. Pulvinar nec ante aenean,
+              est eu fusce, consequat nisl lorem habitant elit suspendisse. Ut tortor metus, tempus ullamcorper,
+              vehicula lorem euismod venenatis mollis. Diam habitant duis, suscipit nostra, eu etiam orci sollicitudin
+              ligula.
+            </p>
+            <p>
+              Cras commodo metus fusce, dapibus lobortis urna, quisque erat magna vel ullamcorper mattis. Praesent
+              lectus laoreet aenean, senectus amet nec, quisque est lobortis volutpat habitant fringilla porttitor nam.
+              Hendrerit et feugiat semper, torquent leo lacinia, amet urna pellentesque lacus ac lectus orci etiam.
+              Curae euismod praesent, eu vivamus urna placerat ipsum.
+            </p>
+          </div>
+        </div>
+      </article>
     </section>
   </div>
 </template>
